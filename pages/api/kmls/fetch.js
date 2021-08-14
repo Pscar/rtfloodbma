4ac2fed5be9ce4e4850334bbab83327914e2bdb4
Supakon Katika <pangza115@hotmail.com>
2021-08-14 08:55:44 +0700

    map  polygons

--- pages/api/kmls/fetch.js
+++ pages/api/kmls/fetch.js
@@ -1,98 +1,108 @@
-import dbConnect from '../../../utils/dbConnect';
-import Kml from '../../../models/Kml';
-import datas from './o2s6uc.json';
+import dbConnect from "../../../utils/dbConnect";
+import Kml from "../../../models/Kml";
+import datas from "./o2s6uc.json";
 
 let doc = datas.kml.Document;
 
-function data_center_of_map() {
-  const center_of_map = doc[0].LookAt.map(doc_item => {
+const dataCenterOfMap = () => {
+  const center_of_map = doc[0].LookAt.map((doc_item) => {
     let param_doc = [];
     param_doc = doc_item;
-    return param_doc
-  })
-  return center_of_map
-}
+    return param_doc;
+  });
+  return center_of_map;
+};
 
-function data_polygons() {
-  const polygons = doc[0].Folder[4].Placemark.map(blue_item => {
+const dataPolygons = () => {
+  const polygons = doc[0].Folder[4].Placemark.map((blue_item) => {
     let param_blue = [];
     param_blue = blue_item;
 
-    let str = param_blue.MultiGeometry[0].Polygon[0].outerBoundaryIs[0].LinearRing[0].coordinates[0].toString();
+    let str =
+      param_blue.MultiGeometry[0].Polygon[0].outerBoundaryIs[0].LinearRing[0].coordinates[0].toString();
     let datas_str = str.split("\r\n");
-    let result_datas = datas_str.filter(data_str => data_str !== undefined && data_str !== '');
+    let result_datas = datas_str.filter(
+      (data_str) => data_str !== undefined && data_str !== ""
+    );
 
-    let res_datas = result_datas.map(item => {
-      let temp = item.trim().split(",")
+    let res_datas = result_datas.map((item) => {
+      let temp = item.trim().split(",");
       return {
-        "longitude": temp[0],
-        "latitude": temp[1],
-      }
-    })
+        longitude: temp[0],
+        latitude: temp[1],
+      };
+    });
     return {
-      "name": param_blue.name[0],
-      "coordinates": res_datas
-    }
-  })
-  return polygons
-}
+      name: param_blue.name[0],
+      coordinates: res_datas,
+    };
+  });
+  return polygons;
+};
 
-function data_boundary() {
-  let str = doc[0].Folder[6].Placemark[0].MultiGeometry[0].LineString[0].coordinates[0].toString();
+const dataBoundary = () => {
+  let str =
+    doc[0].Folder[6].Placemark[0].MultiGeometry[0].LineString[0].coordinates[0].toString();
   let datas_str = str.split("\r\n");
-  let result_datas = datas_str.filter(data_str => data_str !== undefined && data_str !== '');
+  let result_datas = datas_str.filter(
+    (data_str) => data_str !== undefined && data_str !== ""
+  );
 
-  let res_datas = result_datas.map(item => {
-    let temp = item.trim().split(",")
+  let res_datas = result_datas.map((item) => {
+    let temp = item.trim().split(",");
     return {
-      "longitude": temp[0],
-      "latitude": temp[1],
-    }
-  })
+      longitude: temp[0],
+      latitude: temp[1],
+    };
+  });
   return {
-    "coordinates": res_datas
-  }
-}
+    coordinates: res_datas,
+  };
+};
 
-async function data_to_jsons() {
+const dataToJson = async () => {
+  const data_center = dataCenterOfMap();
+  const data_bon = dataBoundary();
+  const data_poly = dataPolygons();
 
-  const data_center = data_center_of_map();
-  const data_bon = data_boundary();
-  const data_poly = data_polygons();
-
-  const input = { rains: {}, waters: {}, dem: {} }
-  const center = { latitude: data_center[0].latitude[0], longitude: data_center[0].longitude[0] }
+  const input = { rains: {}, waters: {}, dem: {} };
+  const center = {
+    latitude: data_center[0].latitude[0],
+    longitude: data_center[0].longitude[0],
+  };
 
   const myObj = {
+    _id: String,
     input: input,
     center_of_map: center,
     polygons: data_poly,
-    boundary: data_bon
+    boundary: data_bon,
   };
 
   const res_kmls = await Kml.updateOne({ _id: 1 }, myObj, { upsert: true });
   res_kmls.n;
   res_kmls.nModified;
   return res_kmls;
-}
-export default async function handler(req, res) {
-  const { method } = req
+};
 
-  await dbConnect()
+export default async function handler(req, res) {
+  const { method } = req;
+
+  await dbConnect();
 
   switch (method) {
-    case 'GET':
+    case "GET":
       try {
-        const json = await data_to_jsons();
-        res.status(200).json({ success: true, data: json })
+        const json = await dataToJson();
+        res.status(200).json({ success: true, data: json });
       } catch (error) {
-        console.log("catch", error)
-        res.status(400).json({ success: false })
+        console.log("catch", error);
+        res.status(400).json({ success: false });
       }
-      break
+      break;
     default:
-      console.log("default")
-      res.status(400).json({ success: false })
-      break
+      console.log("default");
+      res.status(400).json({ success: false });
+      break;
   }
 }
