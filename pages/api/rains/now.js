0159eeb00975389d9bfd0a1b3175ca540c798112
pscar <pangza115@hotmail.com>
2021-09-11 21:54:26 +0700

    api now

---
+++ pages/api/rains/now.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import dbConnect from '../../../utils/dbConnect';
+import Rain from '../../../models/Rain';
+
+const get_rain = async () => {
+  try {
+
+    const req = axios.get('http://api2.thaiwater.net:9200/api/v1/thaiwater30/public/rain_24h');
+    const res = await req;
+    const datas = res.data.data;
+    let data = datas.filter(data => data.geocode.amphoe_name.th === 'ทุ่งสง');
+    const items = data.slice(-1);
+
+    for (let item of data) {
+      item._id = `${item.id}_${item.station.id}`;
+    }
+
+    return items;
+  } catch (error) {
+    console.log(error)
+  }
+}
+const update_rain = async () => {
+
+  let get_rains = await get_rain();
+
+  for (let item of get_rains) {
+    const res_rains = await Rain.updateOne({ _id: item._id }, item, { upsert: true });
+    res_rains.n;
+    res_rains.nModified;
+  }
+
+  return get_rains
+}
+
+export default async function handler(req, res) {
+  const { method } = req
+
+  await dbConnect()
+
+  switch (method) {
+    case 'GET':
+      try {
+        /* find all the data in our database */
+        const update_rains = await update_rain()
+        res.status(200).json({ success: true, data: update_rains })
+      } catch (error) {
+        console.log("catch", error)
+        res.status(400).json({ success: false })
+      }
+      break
+    default:
+      console.log("default")
+      res.status(400).json({ success: false })
+      break
+  }
+
+}
