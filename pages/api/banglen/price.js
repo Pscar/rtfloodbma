8825efe516712163e64c6c711bb9daabb785c0e7
Chavalit Koweerawong <chavalit.kow@gmail.com>
2021-12-27 09:49:38 +0700

    Fix HOST

--- pages/api/banglen/price.js
+++ pages/api/banglen/price.js
@@ -7,12 +7,14 @@ export default async function handler(req, res) {
     to_date= to_date ? to_date : "2021-01-31";
     console.log(product_id, from_date, to_date );
     // const url = "https://dataapi.moc.go.th/gis-product-prices?product_id="+product_id+"&from_date="+from_date+"&to_date="+to_date;
-    const url = "http://localhost:3000/json/rice/"+product_id+".json";
+    // const url = "http://localhost:3000/json/rice/"+product_id+".json";
+    const url = process.env.HOST+"/json/rice/"+product_id+".json";
     let promiseA = await fetch(url);
     let data = await promiseA.json();
     // let filtered_data = data.waterlevel_data.data.filter(function (item) {
     //     return item.geocode.province_name.th == "นครปฐม";
     // });
+    // console.log(process.env);
     res.status(200).json(data);
     // res.status(200).send("Hello World");
 }
--- pages/api/banglen/product.js
+++ pages/api/banglen/product.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 export default async function handler(req, res) {
     // const url = "https://dataapi.moc.go.th/gis-products?keyword=ข้าว&sell_type=wholesale";
 
-    const url = "http://localhost:3000/json/product-rice.json";
+    // const url = "http://localhost:3000/json/product-rice.json";
+    const url = process.env.HOST+"/json/product-rice.json";
     let promiseA = await fetch(encodeURI(url));
     let data = await promiseA.json();
     let filtered_data = data.filter(function (item) {
